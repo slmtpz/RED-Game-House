efc9816390c965e3092cc5427777ffacfa94ebcc
A.Seleme Topuz <A.Seleme Topuz>
2016-12-29 20:46:28 +0300

    ui design and model design for gameslot have been done

--- GameSlot.py
+++ GameSlot.py
@@ -1,11 +1,39 @@
 from tkinter import *
 
 
-class GameSlot:
+class GameSlot():
 
-    def __init__(self, master):
-        #frame = Frame(master, width=200, height=160, bg="red")
-        #frame.pack()
+    def __init__(self, master, slot_number, game_type):
 
-        master.config(highlightbackground="red")
 
+        #top_frame = Frame(self.frame)  # have to be self?
+        #top_frame.pack()
+        #bottom_frame = Frame(self.frame)
+        #bottom_frame.pack(side=BOTTOM)
+
+
+        self.frame = master
+        self.frame.grid_propagate(0)
+
+        self.number_label = Label(self.frame, text=slot_number, bg="red")
+        self.number_label.grid(row=0, column=0)
+
+        self.game_type_label = Label(self.frame, text=game_type['name'], bg="green")
+        self.game_type_label.grid(row=0, column=1)
+
+        self.total_charge = 0.00
+        self.charge_label = Label(self.frame, text=self.total_charge, bg="blue")
+        self.charge_label.grid(columnspan=2)
+
+        self.status = 0
+
+        self.delog_frame_sizes()
+
+    def activate(self):
+        self.status = 1
+        self.frame.config(highlightbackground="red")
+
+    def delog_frame_sizes(self):
+        self.frame.update()
+        print(self.frame.winfo_height())
+        print(self.frame.winfo_width())
--- root.py
+++ root.py
@@ -1,30 +1,52 @@
 from tkinter import *
 from GameSlot import *
+import random
 
 
-number_of_game_slots = 15
+number_of_game_slots = 14
 game_slots = []
-grid_size = 25
+grid_size = 23
+max_game_slots_in_one_row = 5
 game_slot_places = [1, 1, 1, 1, 1,
                     1, 0, 0, 0, 1,
                     0, 0, 1, 1, 1,
                     0, 0, 1, 1, 1,
-                    1, 1, 1, 0, 0]
-game_types = ['Playstation', 'Guitar Hero']
+                    1, 1, 0]
+game_types = [
+    {
+        'name': 'Playstation',
+        'charge': 5.00
+    },
+    {
+        'name': 'Guitar Hero',
+        'charge': 7.00
+    },
+    {
+        'name': 'Pool',
+        'charge': 10.00
+    }
+]
 
 
 root = Tk()
 root.wm_title("RED Game House")
 
+
 game_slot_frames = []
 for i in range(0, grid_size):
     frame = Frame(root, width=200, height=160, highlightbackground="black", highlightthickness=2)
-    frame.grid(row=int(i/5), column=i % 5, padx=10, pady=10)
+    frame.grid(row=int(i/max_game_slots_in_one_row), column=i % max_game_slots_in_one_row, padx=10, pady=10)
     game_slot_frames.append(frame)
 
 for i in range(grid_size):
     if game_slot_places[i] == 1:
-        game_slot = GameSlot(game_slot_frames[i])
+        game_slot = GameSlot(game_slot_frames[i], i+1, random.choice(game_types))
+        #game_slot.activate()
         game_slots.append(game_slot)
 
+root.update()
+print("height")
+print(root.winfo_height())
+print("width")
+print(root.winfo_width())
 root.mainloop()
