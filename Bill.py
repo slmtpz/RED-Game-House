e39fad1503ba8b94bacc49ce842ffcd13f04dad5
slmtpz <seleme94@hotmail.com>
2017-01-05 19:19:51 +0300

    Bill class added

---
+++ Bill.py
@@ -0,0 +1,18 @@
+class Bill:
+
+    def __init__(self):
+        self.games = []
+        self.consumptions = []
+        self.others = []
+        self.total_charge = 0.0
+        self.is_active = False
+
+    def add_game(self, game_type, number_of_players, time_passed_in_sec):
+        pass
+
+    def add_consumption(self):
+        pass
+
+    def add_other(self):
+        pass
+
